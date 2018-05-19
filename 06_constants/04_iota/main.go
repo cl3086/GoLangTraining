5888e5c3a50e42ee0d1e12e7c5949ce6d4b2e5a9
Calvin Lin <cl3086@nyu.edu>
2018-05-19 02:58:15 -0400

    Lesson 6- constants

---
+++ 06_constants/04_iota/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// saying iota once, rest will also be iota
+const (
+	a = iota // 0
+	b        // 1
+	c        // 2
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
