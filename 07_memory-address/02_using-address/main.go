1d183c0e44052938cd19fddf89f2f63565ef8ab6
Calvin Lin <cl3086@nyu.edu>
2018-05-19 14:49:48 -0400

    Forgot a lesson in lesson 7

--- 07_memory-address/02_using-address/main.go
+++ 07_memory-address/02_using-address/main.go
@@ -7,6 +7,7 @@ const metersToYards float64 = 1.09361
 func main() {
 	var meters float64
 	fmt.Print("Enter meters swam: ")
+	// note that we are passing the memory address of meters into the scan
 	fmt.Scan(&meters)
 	yards := meters * metersToYards
 	fmt.Println(meters, " meters is ", yards, " yards.")
